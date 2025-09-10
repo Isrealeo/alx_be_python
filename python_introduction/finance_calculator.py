05935877ca3a73a75718a6d010328ee9a1463cf9
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-09-11 00:41:36 +0100

    fix bug

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-monthly_incomme = 5000
+monthly_incomme = input ("Enter your monthly income: ")
 monthly_expense = 4000
 mmonthly_saving = monthly_incomme - monthly_expense
-projected_saving = mmonthly_saving * 12 + (mmonthly_saving * 0.05 * 12)
+projected_saving = float(mmonthly_saving) * 12 + float(mmonthly_saving * 0.05 * 12)
 print("Your projected saving after one year is:", projected_saving)
--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
 age = input("How old are you?: ")
-future_year = input("Enter a year in the future: ")
+future_year = input("what year are you in the future?: ")
 future_age = int(future_year) - 2023 + int(age)
 print("in ", future_year, "You will be", future_age, "years old")
