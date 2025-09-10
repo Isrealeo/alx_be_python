678d403cea6e5e25cd0fb6c2994eda5341ab1edd
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-09-11 00:00:33 +0100

    introduction to python

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+age = input("How old are you?: ")
+future_year = input("Enter a year in the future: ")
+future_age = int(future_year) - 2023 + int(age)
+print("in ", future_year, "You will be", future_age, "years old")
