b8cc4a1dd4c83dace79f17d755680d1feaa02aba
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-09-11 00:57:02 +0100

    correct typo

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-current_age = input("How old are you?: ")
+current_age = input("How old are you?")
 future_year = 2025
 future_age = int(future_year) - 2023 + int(current_age)
 print("in ", future_year, "You will be", future_age, "years old")
