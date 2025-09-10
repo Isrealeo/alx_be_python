a304f306910150ec25f07c50cc72c3d8053b053f
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-09-11 00:48:06 +0100

    fix bug

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-monthly_incomme = input ("Enter your monthly income: ")
+monthly_income = input ("Enter your monthly income: ")
 monthly_expense = input("Enter your monthly expense: ")
-mmonthly_saving = monthly_incomme - monthly_expense
+mmonthly_saving = monthly_income - monthly_expense
 projected_saving = (mmonthly_saving) * 12 + (mmonthly_saving * 12 * 0.05)
 print("Your projected saving after one year is:", projected_saving)
