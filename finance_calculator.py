4f0b5125cbb8ed5a9174418cb05d880cec69c7d3
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-09-11 00:28:55 +0100

    relocate file to python_introduction directory

--- finance_calculator.py
+++
@@ -1,5 +0,0 @@
-monthly_incomme = 5000
-monthly_expense = 4000
-mmonthly_saving = monthly_incomme - monthly_expense
-projected_saving = mmonthly_saving * 12 + (mmonthly_saving * 0.05 * 12)
-print("Your projected saving after one year is:", projected_saving)
--- future_age_calculator.py
+++
@@ -1,4 +0,0 @@
-age = input("How old are you?: ")
-future_year = input("Enter a year in the future: ")
-future_age = int(future_year) - 2023 + int(age)
-print("in ", future_year, "You will be", future_age, "years old")
