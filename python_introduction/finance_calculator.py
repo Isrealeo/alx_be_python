5adebaa5c8164be79a0c6f8d624c144627d6c5e0
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-09-11 00:45:29 +0100

    fix bug

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
 monthly_incomme = input ("Enter your monthly income: ")
-monthly_expense = 4000
+monthly_expense = input("Enter your monthly expense: ")
 mmonthly_saving = monthly_incomme - monthly_expense
-projected_saving = float(mmonthly_saving) * 12 + float(mmonthly_saving * 0.05 * 12)
+projected_saving = (mmonthly_saving) * 12 + (mmonthly_saving * 12 * 0.05)
 print("Your projected saving after one year is:", projected_saving)
