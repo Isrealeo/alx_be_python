82b5e75221e6b30116d49a9e98ef748a22b366fe
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-09-10 23:46:40 +0100

    introduction to python

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 30
+number2 = 10
+sum = number1 + number2
+print("addition of", number1 , "and", number2, "is", sum)
+subtraction = number2 - number1
+print("subtraction of", number1 , "and", number2, "is", subtraction)
+multiplication = number1 * number2
+print("multiplication of", number1 , "and", number2, "is", multiplication)
+division = number1 / number2        
