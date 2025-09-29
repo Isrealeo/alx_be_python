b45154388c8d48180204218be487364d2bf58416
Nwaminogbe Isreal Ohunmaehuni <nwaminogbeisreal@gmail.com>
2025-09-29 15:10:29 +0100

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 number =  input("Enter a number to see its multiplication table: ")
 number = int(number)
-for i in range(0, 9):
+for i in range(1, 11):
     result = number * i
     print(f"{number} x {i} = {result}") 
     
