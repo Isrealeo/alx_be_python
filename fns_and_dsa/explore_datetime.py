a689b32cdc0f4343273f9d9711b680a8ee9ba81a
Nwaminogbe Isreal Ohunmaehuni <nwaminogbeisreal@gmail.com>
2025-10-11 11:20:09 +0100

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    print("Todays date is: ", current_date.strftime("%Y-%m-%d"))
+    print("Todays date is: ", current_date.strftime("%Y-%m-%d %H:%M:%S"))
     return current_date
     
 def calculate_future_date(current_date, days_to_add):
