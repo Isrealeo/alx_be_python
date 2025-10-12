642423f1fde52c8c15b26becb058d1ccadf0e9fe
Nwaminogbe Isreal Ohunmaehuni <nwaminogbeisreal@gmail.com>
2025-10-12 01:22:06 +0100

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,10 +1,10 @@
 class Book:
-    def __init__ (self, title:str, author:str):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
-    
+
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
@@ -13,7 +13,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"E-Book: {self.title} by {self.author}, File Size: {self.file_size}MB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
@@ -22,20 +22,16 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"Print Book: {self.title} by {self.author}, Pages: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 
 class Library:
     def __init__(self):
-        self.books = []  # no argument needed
+        self.books = []
 
     def add_book(self, book: Book):
         self.books.append(book)
 
     def list_books(self):
-        if not self.books:
-            print("No books in the library yet.")
-        else:
-            for book in self.books:
-                print(book)
-
-        
+        for book in self.books:
+            print(book)
